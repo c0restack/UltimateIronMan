09224255bb1e80a9d6acf8f779a1eb9d2cf950d1
C0reB <eeronpoika.haapala@gmail.com>
2022-09-20 02:43:07 +0300

    version 1.1

--- src/main/java/ultimateironmanplugin/ultimateironmanplugin/UltimateIronManPlugin.java
+++ src/main/java/ultimateironmanplugin/ultimateironmanplugin/UltimateIronManPlugin.java
@@ -2,7 +2,10 @@ package ultimateironmanplugin.ultimateironmanplugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
@@ -18,6 +21,8 @@ import java.util.Scanner;
 
 public final class UltimateIronManPlugin extends JavaPlugin implements Listener {
 
+    String polku = "C:/Users/eeron/Desktop/papertestserver/plugins/";
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
@@ -31,10 +36,10 @@ public final class UltimateIronManPlugin extends JavaPlugin implements Listener 
         System.out.println(name + " is being processed");
        // BufferedReader br = null;
        try{
-            File myObj = new File("C:/Users/eeron/Desktop/papertestserver/plugins/"+ name +".txt");
+            File myObj = new File(polku + name +".txt");
             if (myObj.createNewFile()){
                 try {
-                    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("/C:/Users//eeron//Desktop//papertestserver//plugins//"+name+".txt", false)));
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(polku +name+".txt", false)));
                     out.print(0);
                     out.close();
                 } catch (IOException e) {
@@ -49,29 +54,29 @@ public final class UltimateIronManPlugin extends JavaPlugin implements Listener 
             throw new RuntimeException(e);
         }
        //print out file
-        Scanner scanner = new Scanner(new File("C:/Users/eeron/Desktop/papertestserver/plugins/"+ name +".txt"));
+        Scanner scanner = new Scanner(new File(polku + name +".txt"));
         int [] tall = new int [100];
         int deaths = scanner.nextInt();
         if (deaths == 0){
             p.setDisplayName(ChatColor.RED + p.getName());
             p.setPlayerListName(ChatColor.RED + p.getName());
 
+
             Bukkit.broadcastMessage(ChatColor.RED + "ULTIMATE IRONMAN " + name + " ON LIITTYNYT SERVERILLE!");
             for(Player all : Bukkit.getOnlinePlayers()){
-                p.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_1, 5, 0);
-                p.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_2, 5, 0);
-                p.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_0, 5, 0);
+                all.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_1, 5, 0);
+                all.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_2, 5, 0);
+                all.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_0, 5, 0);
             }
 
-        }else {
+        } else if (deaths >= 10) {
+            p.setGameMode(GameMode.SPECTATOR);
+        } else {
             Bukkit.broadcastMessage(ChatColor.BLUE + "Lmao joku nobo joinas");
         }
         System.out.println(name + " on kuollut " + deaths + " kertaa.");
         setMaxHp(deaths, p);
 
-        //check if file exists
-        //create file for player if need
-        //check stats
     }
 
     @EventHandler
@@ -81,17 +86,21 @@ public final class UltimateIronManPlugin extends JavaPlugin implements Listener 
         String name = p.getName();
         //kattoo monesti kuollu
 
-        Scanner scanner = new Scanner(new File("C:/Users/eeron/Desktop/papertestserver/plugins/"+ name +".txt"));
+        Scanner scanner = new Scanner(new File(polku + name +".txt"));
         int [] tall = new int [100];
         int deaths = scanner.nextInt();
         int newDeaths = deaths + 1;
         //kirjaa uuden kuoleman
         try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("/C:/Users//eeron//Desktop//papertestserver//plugins//"+name+".txt", false)));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(polku +name+".txt", false)));
             out.print(newDeaths);
             out.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        if (newDeaths >= 10){
+            p.setGameMode(GameMode.SPECTATOR);
+            Bukkit.broadcastMessage(ChatColor.DARK_GREEN + name + " on kuollu vitusti liian monta kertaa, ettei se saa enään pelata");
         }
 
         setMaxHp(newDeaths, p);
@@ -102,7 +111,58 @@ public final class UltimateIronManPlugin extends JavaPlugin implements Listener 
         int deathLoose = deaths * 2;
         int deathMod = 20 - deathLoose;
         player.setMaxHealth(deathMod);
+        String name = player.getName();
+        if (deaths == 1){
+            Bukkit.broadcastMessage(ChatColor.DARK_GREEN + name + " on menettänyt ULTIMATE IRONMAN statuksensa!");
+            for(Player all : Bukkit.getOnlinePlayers()){
+                all.playSound(all.getLocation(), Sound.ITEM_AXE_STRIP, 5, 0);
 
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+
+        if (command.getName().equalsIgnoreCase("ironman")){
+
+            if (sender instanceof Player){
+                Player p = (Player) sender;
+                String name = p.getName();
+
+                if (args.length == 0){
+
+                    //tarkistaa itsensä
+
+                } else if (args.length > 1) {
+                    p.sendMessage(ChatColor.GOLD + "Voit tarkistaa vaih yhden pelaajan kerrallaan.");
+                }else {
+                    String ironName = args[0];
+                    Scanner scanner = null;
+                    try {
+                        scanner = new Scanner(new File(polku + ironName +".txt"));
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    int [] tall = new int [100];
+                    int deaths = scanner.nextInt();
+                    if (deaths == 0){
+                        p.sendMessage(ChatColor.GREEN+ ironName + " ON ULTIMATE IRONMAN!");
+                    }else {
+                        p.sendMessage(ChatColor.GOLD+ ironName + " ei ole ultimate ironman!");
+                        int hp = 10 - deaths;
+                        p.sendMessage(ChatColor.GOLD+ "Hänellä on "+ hp +" sydäntä jäljellä.");
+                    }
+
+                }
+
+
+            }
+
+        }
+
+        return true;
     }
 
 
